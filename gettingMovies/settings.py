cd484c246443d89e134bfd704497f4e6c65fcd6d
Koech-code <nixonkipkorir01@gmail.com>
2022-03-29 14:13:55 +0300

    end of task 2

--- gettingMovies/settings.py
+++ gettingMovies/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pv^mup$k-91)y2fw16)uxm9g4x*hxkv#io@p^co!e)+4=%=%b$'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
