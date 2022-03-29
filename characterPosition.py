0e6f9f8d039be8f967982efe9927ebfcfb4d6679
Koech-code <nixonkipkorir01@gmail.com>
2022-03-29 13:33:40 +0300

    start of task 1

--- characterPosition.py
+++ characterPosition.py
@@ -1,5 +1,8 @@
+from operator import index
+
+
 name=input('Please input a string e.g your name: ')
 print(name)
 
-for position, character in enumerate(name, start=1):
-    print(character, position)
+for index, character in enumerate(name, start=1):
+    print(character, index)
