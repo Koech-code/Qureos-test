d1d5c138935f7cc0c89af1675cd1290c17f34514
Koech-code <nixonkipkorir01@gmail.com>
2022-03-29 14:13:03 +0300

    end of task 2

--- titles/views.py
+++ titles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def home(request):
+    response = requests.get('https://jsonmock.hackerrank.com/api/movies/search/?Title=spiderman&page=1')
+    moviedata = response.json()
+    return render(request, 'core/home.html', {
+        'data': moviedata['data'],
+        'title': moviedata[sorted('title')]
+    })
